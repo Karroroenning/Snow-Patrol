10c8f12568d3e61a5726bff0924c1e0a363fd0d8
karolina Rönning <Karroroenning@gmail.com>
2023-09-26 12:56:06 +0000

    fix the top header with logo, search, my account and cart.Created static folder. And added couple of settings to link up our static and media files

--- snowpatrol/settings.py
+++ snowpatrol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x1f&(dym4u+3x+$^y(r%8q#9mqh19g-$3_4!9q1mvps$+$b&@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-karroroennin-snowpatrol-9s084sgh059.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-karroroennin-snowpatrol-9s084sgh059.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -146,6 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
