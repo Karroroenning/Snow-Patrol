1b256aa7493a3318706787e932dc8d0bae97feb9
karolina Rönning <Karroroenning@gmail.com>
2023-09-27 14:20:39 +0000

    customized admin panel, product and categories

--- products/models.py
+++ products/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    verbose_name_plural = 'Categories'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
