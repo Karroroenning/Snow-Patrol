976b2747971c692a6808564325b2ca007286c054
karolina Rönning <Karroroenning@gmail.com>
2023-10-02 11:58:47 +0000

    Created the stripe_elements.js file, added some CSS to it so we have an okey card form

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NwkQDAbT5XrHESpxC9qVbxpK4IAz7R7iWcDCGbu2LG5HjQ6DFQ0WgDnRkh5y2gjoTtuxrgNr5R0Vzp20WbDosbo00idJbG7f3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
