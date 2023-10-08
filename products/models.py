a3b0b8b35ebb313146a9041daf208007e21a0f3f
karolina Rönning <Karroroenning@gmail.com>
2023-10-08 18:27:34 +0000

    all my products and categories disappeared so I created fixtures with the products and categories

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-    
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -33,7 +33,7 @@ class Product(models.Model):
         null=True,
         blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(upload_to="shop-images/", null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
