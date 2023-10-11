1dae5cb3824bbfdd92c60e5bd700b849eaf43a3e
karolina Rönning <Karroroenning@gmail.com>
2023-10-11 08:43:47 +0000

    added delete_review and edit_review in the views and fixed the url path to it in the urls.py and in the template

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path(
+        'delete-review/<int:review_id>/',
+        views.delete_review,
+        name='delete_review'),
+    path(
+        'edit-review/<int:review_id>/',
+        views.edit_review,
+        name='edit_review'),
 ]
